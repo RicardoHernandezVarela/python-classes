1ec07b7f796f95a3a78738a8b94cf22620db74f9
RicardoHernandezVarela <rhvarela011192@gmail.com>
2020-02-10 00:36:35 -0600

    Update assigments, units andcourses in group class

--- school.py
+++ school.py
@@ -5,6 +5,13 @@ class Group:
         self.name = name
         self.students = []
         self.courses = []
+        self.total_scores = {}
+
+    def updateCourses(self, course):
+        if course.name not in self.total_scores:
+            self.total_scores[course.name] = []
+        else:
+            pass
 
     def __str__(self):
         return str(self.name) +  ' ' + str(len(self.students)) + ' students'
@@ -17,11 +24,13 @@ class Course:
         self.group = group
         self.units = []
         group.courses.append(self)  # Assign this course to a group.
+        group.updateCourses(self)
 
     # Add unit.
     def addUnit(self, name):
         unit = (name, [])
         self.units.append(unit)
+        self.group.total_scores[self.name].append(unit[1])
 
     # Add assigment.
     def addAssigment(self, unit, assignment):
@@ -38,22 +47,21 @@ class Student:
             self.lastname = lastname
             self.group = group
             self.courses = group.courses
-            self.assigments = []
             self.scores = []
             group.students.append(self)  # Assign this student to a group.
-            
-        def updateAssigments(self):
-            # Get all student's assigments.
-            for course in self.courses:
-                for unit in course.units:
-                    for assigment in unit[1]:
-                        if assigment not in self.assigments:
-                            self.assigments.append(assigment)
-                            self.scores.append(0)
-                        else:
-                            pass
+        
+        def updateAssigments(self, course, unit):
+            print("")
+            """
+            for assigment in course.units[unit]:
+                if assigment not in self.assigments:
+                    self.assigments.append(assigment)
+                    self.scores.append(0)
+                else:
+                    pass
                 
-                print("Assigments list updated", self.assigments)
+                print("Unit's assigments list updated", self.assigments)
+            """
 
         def addScore(self, index, score):
             self.scores[index] = (score)
@@ -87,7 +95,7 @@ def main():
     robotics = Course("Robotics", tenth)
     print('New Course created {} in {}'.format(robotics.name, tenth.name))
 
-    # Create a unit and assign it a course.
+    # Create a unit and assign to a course.
     robotics.addUnit("First")
     print('New unit created in {} course'.format(robotics.name))
 
@@ -104,7 +112,7 @@ def main():
     ricardo = Student("Ricardo", "Varela", tenth)
     
     # Update assigmnets of a student.
-    ricardo.updateAssigments()
+    ricardo.updateAssigments(robotics, 0)
 
     assigment_2 = {
         "title": "Arduino conditionals",
@@ -113,14 +121,36 @@ def main():
 
     robotics.addAssigment(0, assigment_2)
 
-    ricardo.updateAssigments()
+    ricardo.updateAssigments(robotics, 0)
 
+    # Create a new course, check if update works.
+    Math = Course("Math", tenth)
+
+    # Create a unit and assign to a course, check if update works.
+    robotics.addUnit("Second")
+    print('New unit created in {} course'.format(robotics.name))
+
+    Math.addUnit("First")
+    print('New unit created in {} course'.format(Math.name))
+
+    # Create an assigment for a unit.
+    assigment_3 = {
+        "title": "Solve equations",
+        "points": 1.2
+    }
+
+    Math.addAssigment(0, assigment_3)
+    print('{} assigment added to {} unit of {} course'.format(assigment_3['title'], Math.units[0][0], Math.name))
+
+    print(tenth.total_scores)
+
+"""
     # Find the index of a assigment(dict) in the assigments array.
     print(ricardo.assigments.index(assigment_1))
 
-    a = Score(ricardo, assigment_2, 10)
-    print(a.index)
-    print(ricardo.scores)
+    first_score = Score(ricardo, assigment_2, 10)
+    print('Student {} was assigned {} to {} assigment'.format(ricardo.name, first_score.total, assigment_2['title']))
+"""
 
 #Call main() function.
 if __name__ == '__main__':
